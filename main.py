e1991e70064f275ab7df0540441b5e5faf8aa31c
Nagendra Ramaswami <nagendra.ramaswami@asterquanta.com>
2022-07-21 23:54:42 +0530

    Added another route /sensors

--- main.py
+++ main.py
@@ -4,3 +4,11 @@ app = FastAPI()
 @app.get("/")
 def hello():
     return {"message":"Hello nCorium APIs"}
+
+@app.get("/sensors")
+def list_sensors():
+    return [
+        {"id" : "001", "name" : "Main Entrance"},
+        {"id" : "002", "name" : "Basement 2 Parking"},
+        {"id" : "003", "name" : "Rear Entrance"}
+    ]
